2cc9f9e917ae0d9929a2b026b61d63055d89f6cf
Karim-Kanaan <113034405+Karim-Kanaan@users.noreply.github.com>
2022-09-27 09:15:14 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
 input.onButtonPressed(Button.A, function () {
     counter += 1
 })
+// When both are pressed the microbit lights up to show the final value of the variable
 input.onButtonPressed(Button.AB, function () {
     basic.showNumber(counter)
 })
+// When B is pressed it changes the value of the variable by negative one
 input.onButtonPressed(Button.B, function () {
     counter += -1
 })
